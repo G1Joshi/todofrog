352cffdeaa8d4d79838001d0be645219ece24b01
Jeevan Joshi <joshijeevan98@gmail.com>
2022-11-23 19:28:02 +0530

    update api endpoints

--- todofrog_client/lib/config/apis.dart
+++ todofrog_client/lib/config/apis.dart
@@ -1,4 +1,4 @@
 const String baseUrl = 'http://localhost:8080/';
-const String loginPath = '/login';
-const String registerPath = '/register';
+const String loginPath = '/auth/login';
+const String registerPath = '/auth/register';
 const String todoPath = '/todo';
